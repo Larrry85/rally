357ff32fa59cc73c83c8ff9b9afef4d58b395505
lauralevisto <levisto.laura@gmail.com>
2024-09-23 19:12:59 +0300

    driver style

--- public/js/race-flags.js
+++ public/js/race-flags.js
@@ -6,11 +6,11 @@ socket.emit('getRaceFlags');
 // Display race flags
 socket.on('raceFlags', (flag) => {
     const flagsDiv = document.getElementById('flags');
-    flagsDiv.innerHTML = `<p>${flag} Flag</p>`;
+    flagsDiv.innerHTML = `<p>${flag} </p>`;
 });
 
 // Update race flags in real-time
 socket.on('raceFlags', (flag) => {
     const flagsDiv = document.getElementById('flags');
-    flagsDiv.innerHTML = `<p>${flag} Flag</p>`;
+    flagsDiv.innerHTML = `<p>${flag} </p>`;
 });
