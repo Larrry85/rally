51d9c27acb908bcc014984470b9951c04e527f64
lauralevisto <levisto.laura@gmail.com>
2024-09-25 14:11:31 +0300

    stupid css

--- public/js/next-race.js
+++ public/js/next-race.js
@@ -1,15 +1,22 @@
-const socket = io();
+document.addEventListener('DOMContentLoaded', () => {
+    const socket = io();
 
-// Request driver list for the next race
-socket.emit('getRaceSessions');
+    const driverListDiv = document.getElementById('driverList');
+    driverListDiv.innerHTML = '<p><strong>Driver List</strong></p><p>Waiting for drivers data...</p>'; // Set default message with header
 
-// Display driver list
-socket.on('raceSessions', (sessions) => {
-    const nextSession = sessions.find(session => session.isNext);
-    if (nextSession) {
-        const driverListDiv = document.getElementById('driverList');
-        driverListDiv.innerHTML = nextSession.drivers.map(driver => `
-            <p><strong>Car:</strong> ${driver.carNumber} <strong> - Driver:</strong> ${driver.driver}</p>
-        `).join('');
-    }
+    // Request driver list for the next race
+    socket.emit('getRaceSessions');
+
+    // Display driver list
+    socket.on('raceSessions', (sessions) => {
+        const nextSession = sessions.find(session => session.isNext);
+        
+        if (nextSession && nextSession.drivers.length > 0) {
+            driverListDiv.innerHTML = '<p><strong>Driver List</strong></p><br>' + nextSession.drivers.map(driver => `
+                <p><strong>Car:</strong> ${driver.carNumber} <strong> - Driver:</strong> ${driver.driver}</p>
+            `).join('');
+        } else {
+            driverListDiv.innerHTML = '<p><strong>Driver List</strong></p><p style="font-size: small;"><small>Waiting for drivers data...</small></p>';
+        }
+    });
 });
