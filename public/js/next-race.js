05014a637c5056495ef6592bfefcea34be54be27
lauralevisto <levisto.laura@gmail.com>
2024-10-04 14:10:12 +0300

    next race

--- public/js/next-race.js
+++ public/js/next-race.js
@@ -10,13 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // Display driver list
     socket.on('raceSessions', (sessions) => { // Listen for 'raceSessions' event from server
         const nextSession = sessions.find(session => session.isNext); // Find the next race session
-        
+
         if (nextSession && nextSession.drivers.length > 0) { // Check if next session exists and has drivers
             driverListDiv.innerHTML = '<p class="listheader"><strong>Driver List</strong></p><br>' + nextSession.drivers.map(driver => `
-                <p><strong>Car:</strong> ${driver.carNumber} <strong> - Driver:</strong> ${driver.driver}</p>
-            `).join(''); // Update driver list with driver details
+                <p>Car:  <strong>${driver.carNumber}</strong>   -    Driver:  <strong>${driver.driver}</strong></p>
+    `).join('') + '<br><br><p style="font-size: large; color: red;"><strong>Please proceed to the paddock.</strong></p>'; // Update driver list with driver details and add paddock message
         } else {
-            driverListDiv.innerHTML = '<p><strong>Driver List</strong></p><p style="font-size: small;"><small>Waiting for drivers data...</small></p>'; // Display waiting message if no drivers
+            driverListDiv.innerHTML = '<p class="listheader"><strong>Driver List</strong></p><p style="font-size: small;"><small>Waiting for drivers data...</small></p>'; // Display waiting message if no drivers
         }
     });
+
 });
