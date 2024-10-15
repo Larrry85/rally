8216d19c3c9e6e3d8da962995f609654180a806a
raigohoim <vikationu@gmail.com>
2024-10-15 08:37:31 +0300

    cleaned up

--- public/js/next-race/handlers.js
+++ public/js/next-race/handlers.js
@@ -3,33 +3,22 @@ import { DOM } from "./dom.js";
 import { CONFIG } from "./config.js";
 
 export function updateDriverList(nextSession) {
-  if (nextSession && nextSession.drivers.length > 0) {
-    DOM.driverListDiv.innerHTML =
-      '<p class="listheader"><strong>Driver List</strong></p><br>' +
-      nextSession.drivers
-        .map(
-          (driver) => `
-        <p>Car:  <strong>${driver.carNumber}</strong>   -    Driver:  <strong>${driver.driver}</strong></p>
-      `
-        )
-        .join("");
-  } else {
-    DOM.driverListDiv.innerHTML = CONFIG.WAITING_MESSAGE;
-  }
+  DOM.driverListDiv.innerHTML =
+    nextSession && nextSession.drivers.length > 0
+      ? `<p class="listheader"><strong>Driver List</strong></p><br>${nextSession.drivers
+          .map(
+            (driver) =>
+              `<p>Car: <strong>${driver.carNumber}</strong> - Driver: <strong>${driver.driver}</strong></p>`
+          )
+          .join("")}`
+      : CONFIG.WAITING_MESSAGE;
 }
 
-export function showPaddockMessage() {
+export function togglePaddockMessage(show) {
   const paddockMessage = DOM.getPaddockMessage();
   if (paddockMessage) {
-    paddockMessage.style.display = "block";
-  } else {
+    paddockMessage.style.display = show ? "block" : "none";
+  } else if (show) {
     DOM.driverListDiv.innerHTML += CONFIG.PADDOCK_MESSAGE;
-  }
-}
-
-export function hidePaddockMessage() {
-  const paddockMessage = DOM.getPaddockMessage();
-  if (paddockMessage) {
-    paddockMessage.style.display = "none";
   }
 }
