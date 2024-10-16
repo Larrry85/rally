f15b7f4a8768927ef9f2fd782a195a7b616b138a
lauralevisto <levisto.laura@gmail.com>
2024-10-16 21:39:17 +0300

    lap tracker login and buttons

--- public/js/lap-line-tracker/dom.js
+++ public/js/lap-line-tracker/dom.js
@@ -2,6 +2,8 @@
 export const DOM = {
   loginScreen: document.getElementById("login"),
   lapLinerApp: document.getElementById("lapLinerApp"),
+  lapLinerMessage: document.getElementById("lapLinerMessage"),
+  lapLinerButtons: document.getElementById("lapLinerButtons"),
   loginButton: document.getElementById("loginButton"),
   accessKeyInput: document.getElementById("accessKey"),
   loginMessage: document.getElementById("loginMessage"),
